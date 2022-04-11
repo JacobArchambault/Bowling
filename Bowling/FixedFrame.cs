d4381a7005e5daca0337901673ccf80e2b65dffa
ppi-jarchambault <82416087+ppi-jarchambault@users.noreply.github.com>
2022-04-11 18:55:42 -0400

    Add FixedFrame class returning a tuple of turns and their sum

---
+++ Bowling/FixedFrame.cs
@@ -0,0 +1,25 @@
+﻿using Bowling.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling
+{
+    internal class FixedFrame
+    {
+        IPlayable _turns;
+
+        public FixedFrame(IPlayable turns)
+        {
+            _turns = turns;
+        }
+
+        internal Tuple<IEnumerable<int>, int> Play()
+        {
+            var turns = _turns.Play();
+            return Tuple.Create(turns, turns.Sum());
+        }
+    }
+}
