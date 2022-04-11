e5c841dbe68fdc7cd9fa30bd05266b3227c4f723
ppi-jarchambault <82416087+ppi-jarchambault@users.noreply.github.com>
2022-04-10 20:55:26 -0400

    Create a Frames class to hold logic for all frames. Move BowlingGame logic into it

--- Bowling/BowlingGame.cs
+++ Bowling/BowlingGame.cs
@@ -2,35 +2,11 @@
 {
     internal class BowlingGame
     {
-        private readonly Frame[] frames = new Frame[10]
-        {
-            new Frame(),
-            new Frame(),
-            new Frame(),
-            new Frame(),
-            new Frame(),
-            new Frame(),
-            new Frame(),
-            new Frame(),
-            new Frame(),
-            new Frame(3)
-        };
-        private int[] scores = new int[10];
+        private readonly Frames _frames = new();
 
         internal void Play()
         {
-            for (int i = 0; i < frames.Length; i++)
-            {
-                Console.Write($"Frame {i + 1}:\t");
-                var roundScores = frames[i].Play();
-                for (int j = 0; j < roundScores.Count(); j++)
-                {
-                    Console.Write($"Roll {j + 1}: {roundScores[j]}\t");
-                }
-                scores[i] = roundScores.Sum();
-                Console.Write($"Round total: {scores[i]}");
-                Console.WriteLine();
-            }
+            _frames.Play();
         }
     }
 }
---
+++ Bowling/Frames.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling
+{
+    internal class Frames
+    {
+        private readonly Frame[] frames = new Frame[10]
+        {
+            new Frame(),
+            new Frame(),
+            new Frame(),
+            new Frame(),
+            new Frame(),
+            new Frame(),
+            new Frame(),
+            new Frame(),
+            new Frame(),
+            new Frame(3)
+        };
+        private int[] scores = new int[10];
+
+        internal void Play()
+        {
+            for (int i = 0; i < frames.Length; i++)
+            {
+                Console.Write($"Frame {i + 1}:\t");
+                var roundScores = frames[i].Play();
+                for (int j = 0; j < roundScores.Count(); j++)
+                {
+                    Console.Write($"Roll {j + 1}: {roundScores[j]}\t");
+                }
+                scores[i] = roundScores.Sum();
+                Console.Write($"Round total: {scores[i]}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
