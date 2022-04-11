5ba690311206558dcc8c5b4d2499f22bed0a7bd4
ppi-jarchambault <82416087+ppi-jarchambault@users.noreply.github.com>
2022-04-11 08:54:53 -0400

    Add FixedFrame class returning int Array rather than List<int>

---
+++ Bowling/FixedFrame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling
+{
+    internal class FixedFrame
+    {
+        private int pins = 10;
+        private readonly int turns;
+        internal FixedFrame(int turns)
+        {
+            this.turns = turns;
+        }
+
+        internal FixedFrame() : this(2) { }
+
+        internal int[] Play()
+        {
+            int[] scores = new int[turns];
+            for (int i = 0; i < turns; i++)
+            {
+                var pinsKnockedDown = new Random().Next(pins + 1);
+                pins -= pinsKnockedDown;
+                scores[i] = pinsKnockedDown;
+                if (pins == 0 && i == 0)
+                {
+                    i = turns; // ensures that on a normal frame, if a player gets a strike, they don't get another turn.
+                }
+            };
+            return scores;
+        }
+    }
+}
