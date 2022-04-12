1d254d04c8f97f89f7c8658f07679843ea515122
ppi-jarchambault <82416087+ppi-jarchambault@users.noreply.github.com>
2022-04-12 09:01:10 -0400

    Add FinalFrame class to solution

---
+++ Bowling/FinalFrame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling
+{
+    internal class FinalFrame
+    {
+        private int pins = 10;
+        private int turns;
+        internal FinalFrame(int turns)
+        {
+            this.turns = turns;
+        }
+
+        internal FinalFrame() : this(2) { }
+
+        internal List<int> Play()
+        {
+            List<int> scores = new();
+            for (int i = 0; i < turns; i++)
+            {
+                var pinsKnockedDown = new Random().Next(pins + 1);
+                pins -= pinsKnockedDown;
+                scores.Add(pinsKnockedDown);
+                if (pins == 0 && turns != 3)
+                {
+                    pins = 10;
+                    turns = 3;
+                }
+
+            };
+            return scores;
+        }
+    }
+}
