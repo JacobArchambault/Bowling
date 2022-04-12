13a64250ce97f5fddf13d08196cf5552035e5ff9
ppi-jarchambault <82416087+ppi-jarchambault@users.noreply.github.com>
2022-04-11 22:04:20 -0400

    Remove commented out code

--- Bowling/BowlingGame.cs
+++ Bowling/BowlingGame.cs
@@ -19,26 +19,6 @@
                 Console.WriteLine();
             }
             Console.WriteLine($"Final Score: {frames.Select(x => x.Item2).Sum()}");
-
-            //for (int i = 0; i < scores.Length; i++)
-            //{
-            //    Console.Write($"Frame {i + 1}:\t");
-            //    for (int j = 0; j < scores[i].Count; j++)
-            //    {
-            //        if (j < scores[i].Count - 1)
-            //        {
-            //            Console.Write($"Roll {j + 1}: {scores[i][j]}\t");
-            //        }
-            //        else
-            //        {
-            //            Console.Write($"Round total: {scores[i][j]}\t");
-            //            finalScore += scores[i][j];
-            //        }
-            //    }
-            //    Console.WriteLine();
-            //}
-            //Console.WriteLine($"Final score: {finalScore}");
-
         }
     }
 }
